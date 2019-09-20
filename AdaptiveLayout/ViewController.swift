4820892420ac7658ed38c52bd69e7beabda50d42
SaulBeto <albert_oct17@hotmail.com>
2019-09-20 09:37:52 -0500

    Add project

---
+++ AdaptiveLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AdaptiveLayout
+//
+//  Created by Saul Alberto Cortez Garcia on 9/18/19.
+//  Copyright © 2019 Saul Alberto Cortez Garcia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
